f6e0ae4f8f66e8c02e415b720d2b200445b5b2ce
saurabhkus7415 <saurabhkus7415@gmail.com>
2024-05-13 16:23:28 +0530

    worked on listing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit, inject } from '@angular/core';
+
+import { MatTableDataSource } from '@angular/material/table';
+
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { LocalService } from './core/service/local.service';
 export interface PeriodicElement {
   name: string;
-  position: number;
   weight: number;
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
-
 @Component({
   selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   standalone: true,
+
   imports: [
     RouterOutlet,
     MatButtonToggleModule,
@@ -35,16 +32,68 @@ const ELEMENT_DATA: PeriodicElement[] = [
     MatButtonModule,
     MatIconModule,
     MatTableModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDividerModule,
   ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-material-project';
+
   hidden = false;
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['name', 'weight', 'symbol'];
+  _localStorage = inject(LocalService);
+  listData: PeriodicElement[] = [];
+  dataSource = new MatTableDataSource<PeriodicElement>(this.listData);
   toggleBadgeVisibility() {
     this.hidden = !this.hidden;
+  }
+  name = new FormControl();
+  weight = new FormControl();
+  symbol = new FormControl();
+
+  ngOnInit(): void {
+    this.onGetData();
+  }
+  onGetData() {
+    console.log(this._localStorage.getData('data') as any);
+    this.listData = this._localStorage.getData('data') as any;
+
+    console.log(this.listData);
+  }
+  onSubmit() {
+    let formData = {
+      name: this.name.value,
+      weight: this.weight.value,
+      symbol: this.symbol.value,
+    };
+    console.log(this.listData);
+
+    console.log(this.listData && this.listData.length);
+
+    if (this.listData && this.listData.length) {
+      this.listData.forEach((element: any) => {
+        if (!element) {
+          this.listData.push(formData);
+        }
+      });
+      this._localStorage.saveData('data', JSON.stringify(this.listData));
+    } else {
+      this.listData = [];
+      this.listData.push(formData);
+      this._localStorage.saveData('data', JSON.stringify(this.listData));
+    }
+    this.onCancel();
+    this.onGetData();
+    console.log(formData, this.listData);
+  }
+
+  onCancel() {
+    this.name.reset();
+    this.weight.reset();
+    this.symbol.reset();
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { importProvidersFrom } from '@angular/core';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { VERSION as CDK_VERSION } from '@angular/cdk';
+import {
+  VERSION as MAT_VERSION,
+  MatNativeDateModule,
+} from '@angular/material/core';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    // provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    importProvidersFrom(MatNativeDateModule),
+  ],
 };
